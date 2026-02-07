122080c9d73ba21bb039e54345792934dde0d20c
Ilnur <ilnur.saipiyev@mail.ru>
2026-02-07 20:44:29 +0500

    Add ERC20 EduToken smart contract

---
+++ contracts/EduToken.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract EduToken is ERC20, Ownable {
+
+    address public crowdfundingContract;
+
+    constructor() ERC20("EduToken", "EDU") Ownable(msg.sender) {}
+    
+
+    modifier onlyCrowdfunding() {
+        require(msg.sender == crowdfundingContract, "Not crowdfunding contract");
+        _;
+    }
+
+    function setCrowdfundingContract(address _contract) external onlyOwner {
+        crowdfundingContract = _contract;
+    }
+
+    function mint(address to, uint256 amount) external onlyCrowdfunding {
+        _mint(to, amount);
+    }
+}
